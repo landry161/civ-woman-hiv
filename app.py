abd2fb0e03e9162f023ed8cff7dff6e619bf9e60
landry161 <bogbe2006@gmail.com>
2024-05-21 00:33:27 +0200

    Update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, flash, render_template,request,redirect,url_for,make_response
 import csv
 import mysql.connector
+import json
 import pandas as pd
 import pdb
+import numpy as np
+from decimal import *
 
 #flask --app app.py --debug run
 
@@ -15,7 +18,61 @@ def home():
 	#loadAndInsertIntoDB()
 	years=getDistinctYear()
 	country=getDistinctCountry()
-	return render_template("index.html",years=years,countries=country)
+	records=countRecords()
+	return render_template("index.html",years=years,records=records,countArray=len(country),countries=country)
+
+@app.route("/stathome",methods=["POST"])
+def getStatHomeByYear():
+	countries = request.form.get("tags")
+	
+	res=getQueryByTags(countries)
+	#pdb.set_trace()
+	return res
+	'''
+	for r in res:
+		print("---Non---")
+		print(r)
+		print(res)
+		print("Oui")
+	print(countries)
+	'''
+	#pdb.set_trace()
+	'''
+	mySQL="""SELECT annee,sum(pourcentage) as total FROM civ group by annee;"""
+	finalStat=[]
+	mySQL="""SELECT count(*) as total FROM civ;"""
+	cursorDB.execute(mySQL)
+	results=cursorDB.fetchall()
+	for val in results:
+		finalStat.append({"year":val[0],"pourcentage":val[1]})
+	return finalStat
+	'''
+
+#Méthode pour la récupération selon les tags
+def getQueryByTags(tagsValue):
+	cursorDB=mydb.cursor()
+	arrayVal=tagsValue.split(",")
+	finalStat=[]
+	for val in range(0,len(arrayVal)):
+		if len(arrayVal[val])>1:
+			mySQL="""SELECT pays, sum(pourcentage) as total FROM civ WHERE pays=%s group by pays;"""
+			value=(arrayVal[val],)
+			cursorDB.execute(mySQL,value)
+			results=cursorDB.fetchall()
+
+			for val in results:
+				finalStat.append({"annee":val[0],"pourcentage":int(val[1])})
+	'''
+	mySQL="""SELECT pays, sum(pourcentage) as total FROM civ WHERE pays=%s group by pays;"""
+	value=(tagsValue,)
+	finalStat=[]
+	cursorDB.execute(mySQL,value)
+	results=cursorDB.fetchall()
+
+	for val in results:
+		finalStat.append({"annee":val[0],"pourcentage":int(val[1])})
+	'''
+	return finalStat
 
 def loadAndInsertIntoDB():
 	print("Insert into DataBase")
@@ -58,6 +115,15 @@ def getDistinctCountry():
 	for y in results:
 		finalCountry.append(y[0])
 	return finalCountry
+
+def countRecords():
+	cursorDB=mydb.cursor()
+	finalCountry=[]
+	mySQL="""SELECT count(*) as total FROM civ;"""
+	cursorDB.execute(mySQL)
+	result=cursorDB.fetchone()	
+	return result[0]
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
